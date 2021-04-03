d96727f626303bec395bd846ad232526d3c3357d
Mitsuharu Goto <mh.goto.web@gmail.com>
2021-04-03 15:40:59 +0900

    feat: importのエイリアス設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AppController } from "@/app.controller";
+import { AppService } from "@/app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import configuration from "./config/configuration";
+import configuration from "@/config/configuration";
 
 @Module({
   imports: [
